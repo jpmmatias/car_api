1553fce1250b71469f08a8ef83d7f3cb8bdf7a23
João Pedro Matias <joao.matias@rebase.com.br>
2022-03-29 21:48:45 -0300

    Refactoring routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,2 +1,10 @@
-export { categoriesRoutes } from './categories.routes';
-export { specificationRoutes } from './specifications.routes';
+import { Router } from 'express';
+import { categoriesRoutes } from './categories.routes';
+import { specificationRoutes } from './specifications.routes';
+
+const router = Router();
+
+router.use('/categories', categoriesRoutes);
+router.use('/specifications', specificationRoutes);
+
+export { router };
