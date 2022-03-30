5cf5886f1e0e64065edacde807daf9d6358b2565
João Pedro Matias <joao.matias@rebase.com.br>
2022-03-30 09:58:54 -0300

    Category importig file

--- src/modules/car/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/car/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,9 +1,47 @@
-import { Multer } from 'multer';
+import fs from 'fs';
+import { parse as csvParse } from 'csv-parse';
 import CategoriesRepository from '../../repositories/implementantions/CategoriesRepository';
 
+interface IImportCategory {
+	name: string;
+	description: string;
+}
 export class ImportCategoryUseCase {
 	constructor(private categoryRepository: CategoriesRepository) {}
-	execute(file: Express.Multer.File) {
-		console.log(file);
+
+	loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+		return new Promise((resolve, reject) => {
+			const categories: IImportCategory[] = [];
+			const stream = fs.createReadStream(file.path);
+
+			const parseFile = csvParse();
+
+			stream.pipe(parseFile);
+
+			parseFile
+				.on('data', async (line) => {
+					const [name, description] = line;
+					categories.push({ name, description });
+				})
+				.on('end', () => {
+					resolve(categories);
+				})
+				.on('error', (err) => {
+					reject(err);
+				});
+		});
+	}
+
+	async execute(file: Express.Multer.File): Promise<void> {
+		const categories = await this.loadCategories(file);
+		categories.map(async (category) => {
+			if (this.categoryDontExists(category.name)) {
+				this.categoryRepository.create(category);
+			}
+		});
+	}
+
+	private categoryDontExists(name: string) {
+		return !this.categoryRepository.findByName(name) === null ? false : true;
 	}
 }
