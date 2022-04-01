522e756988a5ddead89fca4ebdd5ce8da64d9386
João Pedro Matias <joao.matias@rebase.com.br>
2022-04-01 09:20:43 -0300

    Installing TypeORM and Postgress

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
+import 'reflect-metadata';
 import express from 'express';
 import swagger from 'swagger-ui-express';
 import swaggerFile from './swagger.json';
+import './database';
 import { router } from './routes';
 
 const app = express();
